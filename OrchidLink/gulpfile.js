2b52b84ca9a641cbed735b1f91e90be7b288fc7a
Luis Medina <git@lmedinam.es>
2022-11-24 20:41:38 -0400

    Add SASS compiler using Gulp

---
+++ OrchidLink/gulpfile.js
@@ -0,0 +1,26 @@
+﻿import gulp from 'gulp'
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import { deleteAsync } from 'del';
+
+const sass = gulpSass(dartSass);
+
+gulp.task('styles', () => {
+    return gulp.src('resources/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('wwwroot/css/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('resources/sass/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
+
+gulp.task('clean', () => {
+    return deleteAsync([
+        'wwwroot/css/main.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
