7815be9b346bdb35ed3b1e6b5f02f17e90e1ecd6
Luis Medina <git@lmedinam.es>
2022-12-11 08:39:52 -0400

    Add link redirection controller

---
+++ OrchidLink/Controllers/LinkController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrchidLink.Data;
+using System;
+
+namespace OrchidLink.Controllers
+{
+    public class LinkController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public LinkController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("/go/{slug}")]
+        public IActionResult Index(string slug)
+        {
+            var link = _dbContext.Links.Where(d => d.Slug == slug).FirstOrDefault();
+            if (link == null) return NotFound();
+
+            CookieOptions option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(7)
+            };
+            Response.Cookies.Append("OrchidLink.AnnonUser", Guid.NewGuid().ToString(), option);
+
+            return Redirect(link.Destination);
+        }
+    }
+}
